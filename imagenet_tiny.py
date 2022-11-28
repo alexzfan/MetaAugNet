1948ef28aff95bdab896917339db2cd6a544b1c3
Alex Fan <alexfan@stanford.edu>
2022-11-27 23:04:58 -0800

    try

--- imagenet_tiny.py
+++ imagenet_tiny.py
@@ -41,13 +41,13 @@ def load_image(file_path):
     std_image = Compose(
             [
                 Resize(
-                    size=(32, 32)
+                    size=(40, 40)
                 ),        
                 ToTensor(),
                 Normalize(
                     mean=(0.485, 0.456, 0.406), 
                     std=(0.229, 0.224, 0.225)
-                ),
+                )
             ]
         )
     x = std_image(x)
