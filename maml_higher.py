9bdc3692efe6af0dbc14460b63a773dd6dfa47c9
Brian W Hill <brianwhill@mac.com>
2022-12-03 23:54:37 -0800

    Added WandB logging support, eval with mu ltiple augmentations, log_dir name expansion to avoid accidentally overriding.

--- maml_higher.py
+++ maml_higher.py
@@ -10,10 +10,12 @@ import torch.nn.functional as F
 from torch import autograd
 from torch.utils import tensorboard
 from torchvision.models import squeezenet1_1, SqueezeNet1_1_Weights
+import torchvision.transforms as transforms
 import higher
+import wandb
 
 import omniglot
-import imagenet_tiny as imagenet
+# import imagenet_tiny as imagenet
 import util
 import sys
 import random
@@ -21,11 +23,11 @@ import random
 NUM_HIDDEN_CHANNELS = 64
 KERNEL_SIZE = 3
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-SUMMARY_INTERVAL = 10
-SAVE_INTERVAL = 100
+SUMMARY_INTERVAL = 2
+SAVE_INTERVAL = 10
 LOG_INTERVAL = 10
 VAL_INTERVAL = LOG_INTERVAL * 5
-NUM_TEST_TASKS = 600
+NUM_TEST_TASKS = 1 #600
 RESNET_CHANNEL = 3
 INNER_MODEL_SIZE = 4
 
@@ -42,6 +44,7 @@ class MAML:
             pretrain,
             aug_net_size,
             num_augs,
+            train_aug_type,
             inner_lr,
             learn_inner_lrs,
             outer_lr,
@@ -76,6 +79,7 @@ class MAML:
 
         # construct feature extractor
         self._aug_net_size = aug_net_size
+        self.train_aug_type = train_aug_type
         
         self._aug_net = nn.Sequential()
         in_channel = self.num_input_channels
@@ -148,49 +152,49 @@ class MAML:
         self._start_train_step = 0
 
 
-    def _inner_loop(self, images, labels, train):
-        """Computes the adapted network parameters via the MAML inner loop.
+    # def _inner_loop(self, images, labels, train):
+    #     """Computes the adapted network parameters via the MAML inner loop.
 
-        Args:
-            images (Tensor): task support set inputs
-                shape (num_images, channels, height, width)
-            labels (Tensor): task support set outputs
-                shape (num_images,)
-            train (bool): whether we are training or evaluating
+    #     Args:
+    #         images (Tensor): task support set inputs
+    #             shape (num_images, channels, height, width)
+    #         labels (Tensor): task support set outputs
+    #             shape (num_images,)
+    #         train (bool): whether we are training or evaluating
 
-        Returns:
-            parameters (dict[str, Tensor]): adapted network parameters
-            accuracies (list[float]): support set accuracy over the course of
-                the inner loop, length num_inner_steps + 1
-        """
-        accuracies = []
-        inner_parameters = {
-            k: torch.clone(v)
-            for k, v in self._inner_params.items()
-        } 
+    #     Returns:
+    #         parameters (dict[str, Tensor]): adapted network parameters
+    #         accuracies (list[float]): support set accuracy over the course of
+    #             the inner loop, length num_inner_steps + 1
+    #     """
+    #     accuracies = []
+    #     inner_parameters = {
+    #         k: torch.clone(v)
+    #         for k, v in self._inner_params.items()
+    #     } 
 
-        for i in range(self._num_inner_steps):
-            # run resnet on the convnet output
+    #     for i in range(self._num_inner_steps):
+    #         # run resnet on the convnet output
 
-            out = self._inner_forward(images, inner_parameters)
+    #         out = self._inner_forward(images, inner_parameters)
 
-            # get the loss from resnet output
-            loss = F.cross_entropy(out, labels)
-            accuracies.append(util.score(out, labels))
+    #         # get the loss from resnet output
+    #         loss = F.cross_entropy(out, labels)
+    #         accuracies.append(util.score(out, labels))
 
-            d_loss = autograd.grad(outputs = loss, 
-                                    inputs = inner_parameters.values(), 
-                                    create_graph = train)
-            for i, k in enumerate(inner_parameters.keys()):
-                inner_parameters[k] = inner_parameters[k] - self._inner_lrs[k]*d_loss[i]
+    #         d_loss = autograd.grad(outputs = loss, 
+    #                                 inputs = inner_parameters.values(), 
+    #                                 create_graph = train)
+    #         for i, k in enumerate(inner_parameters.keys()):
+    #             inner_parameters[k] = inner_parameters[k] - self._inner_lrs[k]*d_loss[i]
 
-        # last run on the fully adapted params
-        out = self._inner_forward(images, inner_parameters)
-        accuracies.append(util.score(out, labels))
+    #     # last run on the fully adapted params
+    #     out = self._inner_forward(images, inner_parameters)
+    #     accuracies.append(util.score(out, labels))
 
-        return inner_parameters, accuracies
+    #     return inner_parameters, accuracies
 
-    def _outer_step(self, task_batch, train):
+    def _outer_step(self, task_batch, train, aug_type="learned"):
         """Computes the MAML loss and metrics on a batch of tasks.
 
         Args:
@@ -219,7 +223,19 @@ class MAML:
             support_augs = torch.cat([images_support for _ in range(self._num_augs)], dim = 0)
             labels_augs = torch.cat([labels_support for _ in range(self._num_augs)], dim = 0)
 
-            support_augs = self._aug_net(support_augs)
+            if aug_type == 'learned':
+                support_augs = self._aug_net(support_augs)
+            elif aug_type == 'identity':
+                support_augs = support_augs
+            elif aug_type == 'random_crop_flip':
+                image_dim = support_augs.shape[2]
+                random_crop_flip = transforms.Compose([transforms.RandomCrop((image_dim,image_dim), padding=8),transforms.RandomHorizontalFlip()])
+                support_augs = random_crop_flip(support_augs)
+            elif aug_type == 'AutoAugment':
+                augmenter = transforms.AutoAugment(transforms.AutoAugmentPolicy.CIFAR10)
+                support_augs = augmenter(support_augs)
+            else:
+                raise ValueError ("Not a valid augmentation_type")
 
             # use higher
             inner_opt = torch.optim.SGD(self._inner_net.parameters(), lr=1e-1)
@@ -317,7 +333,7 @@ class MAML:
                 accuracies_post_adapt_query = []
                 for val_task_batch in dataloader_val:
                     outer_loss, accuracies_support, accuracy_query = (
-                        self._outer_step(val_task_batch, train=False)
+                        self._outer_step(val_task_batch, train=False, aug_type=self.train_aug_type)
                     )
                     losses.append(outer_loss.item())
                     accuracies_pre_adapt_support.append(accuracies_support[0])
@@ -363,24 +379,29 @@ class MAML:
             if i_step % SAVE_INTERVAL == 0:
                 self._save(i_step)
 
-    def test(self, dataloader_test):
+    def test(self, dataloader_test, args):
         """Evaluate the MAML on test tasks.
 
         Args:
             dataloader_test (DataLoader): loader for test tasks
         """
-        accuracies = []
-        for task_batch in dataloader_test:
-            _, _, accuracy_query = self._outer_step(task_batch, train=False)
-            accuracies.append(accuracy_query)
-        mean = np.mean(accuracies)
-        std = np.std(accuracies)
-        mean_95_confidence_interval = 1.96 * std / np.sqrt(NUM_TEST_TASKS)
-        print(
-            f'Accuracy over {NUM_TEST_TASKS} test tasks: '
-            f'mean {mean:.3f}, '
-            f'95% confidence interval {mean_95_confidence_interval:.3f}'
-        )
+        for aug_type in ['learned','random_crop_flip','AutoAugment']:
+            accuracies = []
+            for task_batch in dataloader_test:
+                _, _, accuracy_query = self._outer_step(task_batch, train=False, aug_type=aug_type)
+                accuracies.append(accuracy_query)
+            mean = np.mean(accuracies)
+            std = np.std(accuracies)
+            mean_95_confidence_interval = 1.96 * std / np.sqrt(NUM_TEST_TASKS)
+            print(
+                f'Augmentation type {aug_type} '
+                f'Accuracy over {NUM_TEST_TASKS} test tasks: '
+                f'mean {mean:.3f}, '
+                f'95% confidence interval {mean_95_confidence_interval:.3f}'
+            )
+            wandb.log({f"{aug_type}_mean_acc": mean})
+            wandb.log({f"{aug_type}_mean_95_confidence_interval":mean_95_confidence_interval})
+
 
     def load(self, checkpoint_step):
         """Loads a checkpoint.
@@ -397,8 +418,8 @@ class MAML:
         )
         if os.path.isfile(target_path):
             state = torch.load(target_path)
-            self._meta_parameters = state['meta_parameters']
-            self._inner_lrs = state['inner_lrs']
+            # self._meta_parameters = state['meta_parameters']
+            # self._inner_lrs = state['inner_lrs']
             self._optimizer.load_state_dict(state['optimizer_state_dict'])
             self._start_train_step = checkpoint_step + 1
             print(f'Loaded checkpoint iteration {checkpoint_step}.')
@@ -422,10 +443,15 @@ class MAML:
 
 
 def main(args):
+    # Initialize logging (Tensorboard and Wandb)
     log_dir = args.log_dir
     if log_dir is None:
-        log_dir = f'./logs/maml/{args.dataset}.way:{args.num_way}.support:{args.num_support}.query:{args.num_query}.inner_steps:{args.num_inner_steps}.inner_lr:{args.inner_lr}.learn_inner_lrs:{args.learn_inner_lrs}.outer_lr:{args.outer_lr}.batch_size:{args.batch_size}'  # pylint: disable=line-too-long
+        log_dir = f'./logs/maml/{args.dataset}.train_aug_type:{args.train_aug_type}.aut_net_size:{args.aug_net_size}.pretrain:{args.pretrain}.num_augs{args.num_augs}.l2_wd{args.l2_wd}.way:{args.num_way}.support:{args.num_support}.query:{args.num_query}.inner_steps:{args.num_inner_steps}.inner_lr:{args.inner_lr}.learn_inner_lrs:{args.learn_inner_lrs}.outer_lr:{args.outer_lr}.batch_size:{args.batch_size}'  # pylint: disable=line-too-long
     print(f'log_dir: {log_dir}')
+    wandb_name = log_dir.split('/')[-1]
+    if args.test : 
+        wandb_name = "eval_" + wandb_name
+    wandb.init(project='meta_aug_net', entity='brianwhill', config=args, name=wandb_name, sync_tensorboard=True)
     writer = tensorboard.SummaryWriter(log_dir=log_dir)
 
     if args.dataset == 'omniglot':
@@ -439,6 +465,7 @@ def main(args):
         args.pretrain,
         args.aug_net_size,
         args.num_augs,
+        args.train_aug_type,
         args.inner_lr,
         args.learn_inner_lrs,
         args.outer_lr,
@@ -516,7 +543,7 @@ def main(args):
             args.num_query,
             NUM_TEST_TASKS
         )
-        maml.test(dataloader_test)
+        maml.test(dataloader_test, args)
 
 
 if __name__ == '__main__':
@@ -558,6 +585,8 @@ if __name__ == '__main__':
                               'training, or for evaluation (-1 is ignored)'))
     parser.add_argument('--debug', default=False, action = 'store_true',
                         help='debug by reducing to base maml')  
+    parser.add_argument('--train_aug_type', default="learned",
+                        help='specity augmentation type for training')  
 
     main_args = parser.parse_args()
     main(main_args)
