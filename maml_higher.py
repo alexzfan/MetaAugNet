8c30b3aaa354ad21ecbfb0f1dce99d8f91e7e9fd
Alex Fan <alexfan@stanford.edu>
2022-12-05 20:53:40 -0800

    print

--- maml_higher.py
+++ maml_higher.py
@@ -309,8 +309,8 @@ class MAML:
                     qry_loss.backward()
 
                     print(self._aug_net)
-                    print(self._aug_net[0].conv_param.weight)
-                    print(self._aug_net[0].conv_param.weight.grad)
+                    print(self._aug_net[0].conv_param.data)
+                    print(self._aug_net[0].conv_param.grad)
                     sys.exit()
 
             # ********************************************************
