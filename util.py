29f3f9667aa39aa3d09f2814c1a4628ebf6cb1ce
Alex Fan <alexfan@stanford.edu>
2022-12-07 19:41:41 -0800

    fix

--- util.py
+++ util.py
@@ -141,10 +141,7 @@ class aug_net_block(nn.Module):
                                              device = DEVICE), 
                                  mean = 0, 
                                  std = 0.1*torch.std(x.detach()).item()
-                                ) \
-                    for _ in range(self.num_augs) \
-                    if random.uniform(0,1) < self.aug_noise_prob \
-                    else nn.init.zeros_like(torch.empty((tB, C, H, W), 
+                                ) for _ in range(self.num_augs) if random.uniform(0,1) < self.aug_noise_prob else nn.init.zeros_like(torch.empty((tB, C, H, W), 
                                              requires_grad = False, 
                                              device = DEVICE)
                                 )
