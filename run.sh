92d716a2bec0b1beacef683a2dd4d6dc8fa62b2b
Brian Hill <35579962+BrianWHill@users.noreply.github.com>
2022-12-09 22:46:05 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # EXPERIMENTS TO RUN 
+# #Eval 
 
 # To run: 
 # bash run.sh
@@ -9,30 +10,32 @@ echo TEST RANDOM INITIALIZATIONS
 python maml_higher.py --outer_lr 1e-3 --num_augs 3 --num_inner_steps 1 --aug_net_size 3 --l2_wd 1e-4 --dataset imagenet --num_train_iterations 5000 --train_aug_type learned --aug_noise_prob 0.1 --identity_init_off
 
 echo TEST NOISE EFFECT
-for aug_noise_prob in 0 0.1 0.5 1 
-do
-    python maml_higher.py --outer_lr 1e-3 --num_augs 3 --num_inner_steps 1 --aug_net_size 3 --l2_wd 1e-4 --dataset imagenet --num_train_iterations 5000 --train_aug_type learned --aug_noise_prob $aug_noise_prob
-done
+python maml_higher.py --outer_lr 1e-3 --num_augs 3 --num_inner_steps 1 --aug_net_size 3 --l2_wd 1e-4 --dataset imagenet --num_train_iterations 5000 --train_aug_type learned --aug_noise_prob 0
+python maml_higher.py --outer_lr 1e-3 --num_augs 3 --num_inner_steps 1 --aug_net_size 3 --l2_wd 1e-4 --dataset imagenet --num_train_iterations 5000 --train_aug_type learned --aug_noise_prob 0.1
+python maml_higher.py --outer_lr 1e-3 --num_augs 3 --num_inner_steps 1 --aug_net_size 3 --l2_wd 1e-4 --dataset imagenet --num_train_iterations 5000 --train_aug_type learned --aug_noise_prob 0.5
+python maml_higher.py --outer_lr 1e-3 --num_augs 3 --num_inner_steps 1 --aug_net_size 3 --l2_wd 1e-4 --dataset imagenet --num_train_iterations 5000 --train_aug_type learned --aug_noise_prob 1
 
 echo TEST num_augs EFFECT WITH NO NOISE 
 python maml_higher.py --outer_lr 1e-3 --num_augs 1 --num_inner_steps 1 --aug_net_size 3 --l2_wd 1e-4 --dataset imagenet --num_train_iterations 5000 --train_aug_type learned --aug_noise_prob 0
 
-echo TEST PRETRAINED 
-python maml_higher.py --outer_lr 1e-3 --num_augs 3 --num_inner_steps 1 --aug_net_size 3 --l2_wd 1e-4 --dataset imagenet --num_train_iterations 5000 --train_aug_type learned --aug_noise_prob 0.1 --pretrained
+# echo TEST PRETRAINED 
+# python maml_higher.py --outer_lr 1e-3 --num_augs 3 --num_inner_steps 1 --aug_net_size 3 --l2_wd 1e-4 --dataset imagenet --num_train_iterations 5000 --train_aug_type learned --aug_noise_prob 0.1 --pretrained
  
 
 echo TEST BASELINES METHODS 
-for num_augs in 3 10 20
-do
-    python maml_higher.py --outer_lr 1e-3 --num_augs $num_augs --num_inner_steps 1 --aug_net_size 3 --l2_wd 1e-4 --dataset imagenet --num_train_iterations 5000 --aug_noise_prob 0.1 --train_aug_type learned
-    python maml_higher.py --outer_lr 1e-3 --num_augs $num_augs --num_inner_steps 1 --aug_net_size 1 --l2_wd 1e-4 --dataset imagenet --num_train_iterations 5000 --aug_noise_prob 0.1 --train_aug_type identity
-    python maml_higher.py --outer_lr 1e-3 --num_augs $num_augs --num_inner_steps 1 --aug_net_size 1 --l2_wd 1e-4 --dataset imagenet --num_train_iterations 5000 --aug_noise_prob 0.1 --train_aug_type random_crop_flip
-    python maml_higher.py --outer_lr 1e-3 --num_augs $num_augs --num_inner_steps 1 --aug_net_size 1 --l2_wd 1e-4 --dataset imagenet --num_train_iterations 5000 --aug_noise_prob 0.1 --train_aug_type AutoAugment
-done
+python maml_higher.py --outer_lr 1e-3 --num_augs 3 --num_inner_steps 1 --aug_net_size 3 --l2_wd 1e-4 --dataset imagenet --num_train_iterations 5000 --aug_noise_prob 0.1 --train_aug_type learned
+python maml_higher.py --outer_lr 1e-3 --num_augs 3 --num_inner_steps 1 --aug_net_size 1 --l2_wd 1e-4 --dataset imagenet --num_train_iterations 5000 --aug_noise_prob 0.1 --train_aug_type identity
+python maml_higher.py --outer_lr 1e-3 --num_augs 3 --num_inner_steps 1 --aug_net_size 1 --l2_wd 1e-4 --dataset imagenet --num_train_iterations 5000 --aug_noise_prob 0.1 --train_aug_type random_crop_flip
+python maml_higher.py --outer_lr 1e-3 --num_augs 3 --num_inner_steps 1 --aug_net_size 1 --l2_wd 1e-4 --dataset imagenet --num_train_iterations 5000 --aug_noise_prob 0.1 --train_aug_type AutoAugment
+
+
+echo TEST HIGH NUM AUGS
+python maml_higher.py --outer_lr 1e-3 --num_augs 10 --num_inner_steps 1 --aug_net_size 3 --l2_wd 1e-4 --dataset imagenet --num_train_iterations 5000 --aug_noise_prob 0.1 --train_aug_type learned
+
 
 echo TEST AUGMENTATION NETWORK SIZE 
-for aug_set_size in 1 3 5 10
-do
-    python maml_higher.py --outer_lr 1e-3 --num_augs 3 --num_inner_steps 1 --aug_net_size $aug_net_size --l2_wd 1e-4 --dataset imagenet --num_train_iterations 5000 --train_aug_type learned --aug_noise_prob 0.1
-done
+python maml_higher.py --outer_lr 1e-3 --num_augs 3 --num_inner_steps 1 --aug_net_size 1 --l2_wd 1e-4 --dataset imagenet --num_train_iterations 5000 --train_aug_type learned --aug_noise_prob 0.1
+python maml_higher.py --outer_lr 1e-3 --num_augs 3 --num_inner_steps 1 --aug_net_size 5 --l2_wd 1e-4 --dataset imagenet --num_train_iterations 5000 --train_aug_type learned --aug_noise_prob 0.1
+python maml_higher.py --outer_lr 1e-3 --num_augs 3 --num_inner_steps 1 --aug_net_size 10 --l2_wd 1e-4 --dataset imagenet --num_train_iterations 5000 --train_aug_type learned --aug_noise_prob 0.1
+
 
